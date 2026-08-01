80b5d72ca302bfce085b1ce240802fe6f00bfcc6
RigSri <srivastavahrige@gmail.com>
2026-08-01 13:46:45 +0530

    feat: configure MongoDB Atlas connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
-const express=require('express');
-const app=express();
-const PORT=5000;
-app.get("/", (req,res)=>{
-    res.send("SkillSync Backend Server is running")
-})
-app.listen(PORT, ()=>{
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectDB = require("./config/db");
+const app = require("./app");
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
